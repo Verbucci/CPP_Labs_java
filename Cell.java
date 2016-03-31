831aad056c4e3c74596be7c83838206031e4dcf2
Verbucci <mistermaxblr@gmail.com>
2016-03-30 22:36:50 -0700

    2048
    Change-Id: Ib0f844b028676e3dd4ea58b311ba3ff1cedddd4e

---
+++ Cell.java
@@ -0,0 +1,134 @@
+package com.ggl.game2048.model;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+
+public class Cell {
+
+  private static final int CELL_WIDTH = 120;
+  private int value;
+  private Point cellLocation;
+
+  public Cell(int value) {
+    setValue(value);
+  }
+
+  public static int getCellWidth() {
+    return CELL_WIDTH;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public boolean isZeroValue() {
+    return (value == 0);
+  }
+
+  public void setCellLocation(int coordinateX, int coordinateY) {
+    setCellLocation(new Point(coordinateX, coordinateY));
+  }
+
+  public void setCellLocation(Point cellLocation) {
+    this.cellLocation = cellLocation;
+  }
+
+  public void draw(Graphics graphics) {
+    if (value == 0) {
+      graphics.setColor(Color.GRAY);
+      graphics.fillRect(cellLocation.x, cellLocation.y, CELL_WIDTH, CELL_WIDTH);
+    } else {
+      Font font = graphics.getFont();
+      FontRenderContext fontRender = new FontRenderContext(null, true, true);
+
+      String text = Integer.toString(value);
+      BufferedImage image = createImage(font, fontRender, CELL_WIDTH, text);
+
+      graphics.drawImage(image, cellLocation.x, cellLocation.y, null);
+    }
+  }
+
+  private BufferedImage createImage(Font font, FontRenderContext fontRender, int width,
+      String text) {
+
+    Font largeFont = font.deriveFont((float) (width / 4));
+    Rectangle2D rectangle = largeFont.getStringBounds(text, fontRender);
+    int rectangleWidth = (int) Math.round(rectangle.getWidth());
+    int rectangleHeight = (int) Math.round(rectangle.getHeight());
+    int rectangleX = (int) Math.round(rectangle.getX());
+    int rectangleY = (int) Math.round(rectangle.getY());
+
+    BufferedImage image = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB);
+
+    Graphics draw = image.getGraphics();
+    draw.setColor(getTileColor());
+    draw.fillRect(0, 0, image.getWidth(), image.getHeight());
+
+    int x = (width / 2) - (rectangleWidth / 2) - rectangleX;
+    int y = (width / 2) - (rectangleHeight / 2) - rectangleY;
+
+    draw.setFont(largeFont);
+    draw.setColor(getTextColor());
+    draw.drawString(text, x, y);
+    draw.dispose();
+    return image;
+  }
+
+  private Color getTileColor() {
+    Color color = Color.WHITE;
+
+    switch (value) {
+      case 2:
+        color = Color.WHITE;
+        break;
+      case 4:
+        color = Color.WHITE;
+        break;
+      case 8:
+        color = new Color(255, 255, 170);
+        break;
+      case 16:
+        color = new Color(255, 255, 128);
+        break;
+      case 32:
+        color = new Color(255, 255, 85);
+        break;
+      case 64:
+        color = new Color(255, 255, 43);
+        break;
+      case 128:
+        color = new Color(255, 255, 0);
+        break;
+      case 256:
+        color = new Color(213, 213, 0);
+        break;
+      case 512:
+        color = new Color(170, 170, 0);
+        break;
+      case 1024:
+        color = new Color(128, 128, 0);
+        break;
+      case 2048:
+        color = new Color(85, 85, 0);
+        break;
+      default:
+        color = new Color(43, 43, 0);
+        break;
+    }
+
+    return color;
+  }
+
+  private Color getTextColor() {
+    return (value >= 256) ? Color.WHITE : Color.BLACK;
+  }
+}
---
+++ HighScoreProperties.java
@@ -0,0 +1,70 @@
+package com.ggl.game2048.properties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import com.ggl.game2048.model.Game2048Model;
+
+// this class provides the file i/o functions to read and store the records
+public class HighScoreProperties {
+
+  private static final String fileName = "game2048.properties";
+  private static final String highCell = "highCell";
+  private static final String highScore = "highScore";
+  private Game2048Model model;
+
+  public HighScoreProperties(Game2048Model model) {
+    this.model = model;
+  }
+
+  public void loadProperties() {
+    Properties properties = new Properties();
+
+    InputStream stream = null;
+    File file = new File(fileName);
+    try {
+      stream = new FileInputStream(file);
+      properties.load(stream);
+      model.setHighScore(Integer.parseInt(properties.getProperty(highScore)));
+      model.setHighCell(Integer.parseInt(properties.getProperty(highCell)));
+    } catch (FileNotFoundException e) {
+
+    } catch (IOException exeption) {
+      exeption.printStackTrace();
+    }
+  }
+
+  public void saveProperties() {
+    Properties properties = new Properties();
+    properties.setProperty(highScore, Integer.toString(model.getHighScore()));
+    properties.setProperty(highCell, Integer.toString(model.getHighCell()));
+
+    OutputStream stream = null;
+    File file = new File(fileName);
+
+    try {
+      stream = new FileOutputStream(file);
+      properties.store(stream, "2048 High Score");
+      stream.flush();
+    } catch (FileNotFoundException exeption) {
+      exeption.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    try {
+      if (stream != null) {
+        stream.close();
+      }
+    } catch (IOException exeption) {
+      exeption.printStackTrace();
+    }
+
+  }
+}
